88c73f5762e3290d5d5a4bbf9486f7ea150897a6
akfangus <akfanakfan@naver.com>
2022-08-03 03:08:52 +0900

    CRUD1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    # 클래스형 view를 넣을때는 .as_view()를 추가해야한다고함.
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
-
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -31,3 +34,12 @@ def hello_world(request):
         # 모델 객체의 모든 정보를 가져옴
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+# class base view가 훨씬 직관적이고 편하다. django의 좋은기능? 같음.
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # reverse 와 reverse_lazy의 차이? : reverse_lazy는 클래스에서만 사용할수 있다. 클래스안에서 reverse는 안됨. reverse는 함수형에서
+    # submit성공했을때 redirect설정
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
